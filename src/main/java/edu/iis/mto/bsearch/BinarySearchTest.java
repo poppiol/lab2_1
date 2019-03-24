5d5ac09672cbd88f17016c9cdcbb9784982fdbb6
Piotr Popiolek <209437@edu.p.lodz.pl>
2019-03-24 20:18:24 +0100

    testy_v1 Dodany test: Dlugosc sekwencji wejsciowej = 1. Elementu wyszukiwanego nie ma w sekwencji.

--- src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -7,9 +7,16 @@ import org.junit.Assert;
 public class BinarySearchTest {
 
     @Test
-    public void searchTest() {
+    public void searchTestElementIsInSeq() {
         int[] seq = {7};
         SearchResult sr = BinarySearch.search(7, seq);
         Assert.assertTrue(sr.isFound());
+    }
+
+    @Test
+    public void searchTestElementIsNotInSeq() {
+        int[] seq = {7};
+        SearchResult sr = BinarySearch.search(4, seq);
+        Assert.assertFalse(sr.isFound());
     }
 }
