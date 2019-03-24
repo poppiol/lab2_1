ebee480f0f538a79d824ebcce00c4a8ad348f919
Piotr Popiolek <209437@edu.p.lodz.pl>
2019-03-24 20:40:25 +0100

    testy_v1 Dodany test: Dlugosc sekwencji wejsciowej > 1. Elementu wyszukiwanego nie ma w sekwencji. Poprawione nazewnictwo Testów.

--- src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -7,37 +7,44 @@ import org.junit.Assert;
 public class BinarySearchTest {
 
     @Test
-    public void searchTestElementIsInSeq() {
+    public void searchTestElementIsInSequenceWithOneElement() {
         int[] seq = {7};
         SearchResult sr = BinarySearch.search(7, seq);
         Assert.assertTrue(sr.isFound());
     }
 
     @Test
-    public void searchTestElementIsNotInSeq() {
+    public void searchTestElementIsNotInSequenceWithOneElement() {
         int[] seq = {7};
         SearchResult sr = BinarySearch.search(4, seq);
         Assert.assertFalse(sr.isFound());
     }
 
     @Test
-    public void searchTestElementIsFirstInSeq() {
+    public void searchTestElementIsFirstInSequenceWithMoreThanOneElement() {
         int[] seq = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         SearchResult sr = BinarySearch.search(0, seq);
         Assert.assertEquals(1, sr.getPosition());
     }
 
     @Test
-    public void searchTestElementIsLastInSeq() {
+    public void searchTestElementIsLastInSequenceWithMoreThanOneElement() {
         int[] seq = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         SearchResult sr = BinarySearch.search(10, seq);
         Assert.assertEquals(seq.length, sr.getPosition());
     }
 
     @Test
-    public void searchTestElementIsInTheMiddleInSeq() {
+    public void searchTestElementIsInTheMiddleInSequenceWithMoreThanOneElement() {
         int[] seq = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         SearchResult sr = BinarySearch.search(5, seq);
         Assert.assertEquals(seq.length / 2 + 1, sr.getPosition());
+    }
+
+    @Test
+    public void searchTestElementIsNotInSequenceWithMoreThanOneElement() {
+        int[] seq = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        SearchResult sr = BinarySearch.search(12, seq);
+        Assert.assertFalse(sr.isFound());
     }
 }
